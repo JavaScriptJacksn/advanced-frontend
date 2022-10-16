4fd84c999e40df69dacd2a494d2792f31a40a01a
JavaScriptJacksn <jbjackson247@gmail.com>
2022-10-16 15:13:18 +0000

    Functional arrow component with props

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting(){
+//   return <h1>Hello</h1>
+// }
+
+const FucntionalGreeting = (props) => <h1>{props.name}</h1>;
+
+export default FucntionalGreeting;
